f7e76da4d14bf6e18fbc3a5099d5bf7b7a24099c
robertmichael0215 <robertmichael0215@outlook.com>
2026-02-03 15:00:26 +0530

    Create bing_meta.js

---
+++ docs/_static/bing_meta.js
@@ -0,0 +1,9 @@
+(function () {
+  var meta = document.createElement("meta");
+  meta.name = "msvalidate.01";
+  meta.content = "739245F5D54BCBF40AC056DC0CBF5710";
+
+  document.getElementsByTagName("head")[0].appendChild(meta);
+
+  console.log("✅ Bing verification meta tag injected successfully");
+})();
