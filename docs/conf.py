fe224f8e5b4e997e193ad0b4a810682f34a3bd08
robertmichael0215 <robertmichael0215@outlook.com>
2026-02-03 14:35:27 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,21 +2,27 @@ project = 'spypoint-login'
 author = 'spypoint-login'
 release = '1.0'
 
+# Extensions
 extensions = [
     'sphinx_sitemap',
 ]
 
+# Paths
 templates_path = ['_templates']
 exclude_patterns = []
 
+# Theme
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-html_js_files = ['chatbot.js']  # Make sure this exists
-html_favicon = '_static/favicon.png'  # Make sure this exists
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # Make sure _static/chatbot.js exists
+html_favicon = '_static/favicon.png'  # Make sure _static/favicon.png exists
 
+# Bing verification (optional)
 html_context = {
     'bing_verification_code': '739245F5D54BCBF40AC056DC0CBF5710'
 }
 
-html_baseurl = 'https://spypointhelpcenter.readthedocs.io/'  # Use real URL
+# Base URL for sitemap
+html_baseurl = 'https://spypointhelpcenter.readthedocs.io/en/latest/'
