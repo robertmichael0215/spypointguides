f4e8cd901d597cab045db5f9594396bed6e36466
robertmichael0215 <robertmichael0215@outlook.com>
2026-02-03 14:16:21 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,15 @@ project = 'spypoint-login'
 author = 'spypoint-login'
 release = '1.0'
 
-extensions = []
+extensions = [
+    'sphinx_sitemap',
+]
+
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'alabaster'
-html_static_path = ['_static']   
+html_static_path = ['_static']
 
 html_js_files = [
     'chatbot.js',
@@ -17,9 +20,5 @@ html_favicon = '_static/favicon.png'
 html_context = {
     'bing_verification_code': '739245F5D54BCBF40AC056DC0CBF5710'
 }
-
-extensions = [
-    'sphinx_sitemap',
-]
 
 html_baseurl = 'https://docs.yourdomain.com/'
